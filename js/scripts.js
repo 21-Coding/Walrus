53dc7e407771e9aedd8dc08035edcb7d4c8f718a
Dominique Youmans and Jake Jensen <jake@>
2019-08-13 11:19:39 -0700

    color change buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,24 @@
 
+// $(document).ready(function() {
+//   $("p").click(function() {
+//     $(".walrus-showing").toggle().slideDown();
+//     $(".walrus-hidden").toggle().slideUp();
+//   });
+// });
+
 $(document).ready(function() {
-  $("p").click(function() {
-    $(".walrus-showing").toggle().slideDown();
-    $(".walrus-hidden").toggle().slideUp();
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
   });
 });
